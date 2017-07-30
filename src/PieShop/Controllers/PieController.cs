c0704601a05d958e675752ce29cd3b7a60f92598
Shubhmeet Kaur <sxk162731@utdallas.edu>
2017-07-30 01:00:30 -0500

    Initial commit

---
+++ src/PieShop/Controllers/PieController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PieShop.Models;
+using PieShop.ViewModels;
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PieShop.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            _pieRepository = pieRepository;
+            _categoryRepository = categoryRepository;
+        }
+        //action method invoked by mVC
+        public ViewResult List()
+        {
+            PieListViewModel pieListViewModel = new PieListViewModel();
+            pieListViewModel.Pies = _pieRepository.Pies;
+            pieListViewModel.CurrentCategory = "Chesse Cake";
+            return View(pieListViewModel);
+        }
+
+        public ViewResult Index()
+        {
+            ViewBag.Message = "Welcome to Meet's Pie Shop";
+            return View();
+        }
+
+    }
+}
---
+++ src/PieShop/Startup.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using PieShop.Models;
+
+namespace PieShop
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //dependency injection
+            services.AddTransient<ICategoryRepository, MockCategoryRepository>();
+            services.AddTransient<IPieRepository, MockPieRepository>();
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            // for showing user defined errors
+            app.UseDeveloperExceptionPage();
+            //allows to handle status code bw 400-600
+            app.UseStatusCodePages();
+            //server static files
+            app.UseStaticFiles();
+            //adding support for mvc, basic route 
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
